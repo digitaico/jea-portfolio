c91a0102d8112c88bf1fe9e6027fb517b4e90c03
Jorge Eduardo Ardila <jea.data@gmail.com>
2025-05-01 18:38:48 -0500

    pruned file. Completed class to print lists of keywords from job listing and from resume

--- ats/KeywordExtractor.py
+++ ats/KeywordExtractor.py
@@ -8,12 +8,10 @@ try:
     nltk.data.find('tokenizers/punkt')
 except nltk.downloader.DownloadError:
     nltk.download('punkt')
-
 try:
     nltk.data.find('corpora/stopwords')
 except nltk.downloader.DownloadError:
     nltk.download('stopwords')
-
 
 class KeywordExtractor:
     """
@@ -37,12 +35,11 @@ class KeywordExtractor:
         @params: text: str
         @returns: str # cleansed text
         """
-
         text = text.lower()
         text = text.translate(str.maketrans('', '', string.punctuation))
         return text
 
-    def extract_keywords(self, text:str) -> list[str]:
+    def extract_keywords(self, text: str) -> list[str]:
         """
         Extract keywords form input text using tokenization, cleansing and removing stop words.
         @params: text: str
@@ -53,4 +50,23 @@ class KeywordExtractor:
         keywords = [word for word in tokens if word not in self.stop_words and word.isalpha()]
         return keywords
 
+# --// Usage
+if __name__ == "__main__":
+    job_text = """
 
+    """
+    resume_text = """
+
+    """
+    # Instantiate the class: to download if not exists stopwords and punkt
+    extractor = KeywordExtractor(language='english')
+
+    job_description_keywords = extractor.extract_keywords(job_text)
+    resume_keywords = extractor.extract_keywords(resume_text)
+
+    # print keywords as visual reference while script runs.
+    print("Job description list of Keywords:")
+    print(job_description_keywords)
+
+    print("resume list of Keywords:")
+    print(resume_keywords)
