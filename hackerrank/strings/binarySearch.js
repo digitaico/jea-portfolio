3f3c5ab63217fd8f041b2875b1607aba8a239327
Jorge Eduardo Ardila <jea.data@gmail.com>
2025-05-28 13:18:54 -0500

    hr problem: finding first and last position

---
+++ hackerrank/strings/binarySearch.js
@@ -0,0 +1,9 @@
+/*
+ * Given an array of integers sorted in non-decreasing order find the starting
+ * and ending position of a given target value.
+ * Use binary search
+ * Find first occurence
+ * find last occurrence
+ *
+ */
+
