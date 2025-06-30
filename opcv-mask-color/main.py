b716c28303def33153f35af7c99c622b06869e42
Jorge Eduardo Ardila <jea.data@gmail.com>
2025-06-30 00:12:45 -0500

    notebook to better visualize steps

--- opcv-mask-color/main.py
+++ opcv-mask-color/main.py
@@ -1,16 +1,20 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-#%matplotlib inline
+#import matplotlib 
 
 print(cv2.__version__)
-QT_DEBUG_PLUGINS=1
-#export QT_QPA_PLATFORM=xcb
 
 imagePath = '1.png'
 image = cv2.imread(imagePath)
 imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-plt.imshow(imageGray)
+#plt.imshow(imageGray)
+
+ret, thresh = cv2.threshold(imageGray, 200, 255, cv2.THRESH_BINARY_INV)
+
+contours, hierarchy = cv2.findContours(image, mode, method[, contours[, 
+                                                                      hierarchy[, 
+                                                                                offset]]])
 
 
